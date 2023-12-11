93b936ff264d4a0d83cea05391f0fc42ef1ef103
libbyou <lcyou95@gmail.com>
2023-12-10 23:43:11 -0600

    uncommented some code

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,8 +15,8 @@ const {
   router.route('/:userId').delete(deleteUser).put(updateUser);
 
   router.route('/:userId').get(getSingleUser);
-  // router.route('/:userId/thoughts').get(getThoughts);
-  // router.route('/:userId/friends').get(getFriends);
+  router.route('/:userId/thoughts').get(getThoughts);
+  router.route('/:userId/friends').get(getFriends);
 
   router.route('/:userId/friends/:friendsId').post(addFriend);
 
